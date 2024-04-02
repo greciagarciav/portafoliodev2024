fe4b6cdf0ddefaca3a5fa5d924a6ee826f369af2
greciagarciav <garciav.grecia@gmail.com>
2024-04-02 17:21:23 -0500

    Update App files

--- src/App.styled.tsx
+++ src/App.styled.tsx
@@ -1,9 +1,9 @@
 import styled from "@emotion/styled"
 import { Box } from "@mui/material"
-
+import { colors } from "./constants/colors.ts"
 
 export const AppContainer = styled(Box)(() => ({
-  // background: colors.lightGray,
+  background: colors.black,
   width: '100%',
   height: 'calc(100vh - 69px)',
   maxHeight: '100vh',
